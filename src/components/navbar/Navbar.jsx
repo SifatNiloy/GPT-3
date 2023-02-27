626c7b9fc9f18a3c69da1fe21aedaa9a8b8d281f
SifatNiloy <sifatniloy18@gmail.com>
2023-02-27 20:42:45 +0600

    css styles added and assets imported

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
 import './Navbar.css';
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className='gpt3__navbar'>
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src={logo} alt="logo" />
+                </div>
+            </div>
         </div>
     );
 };
