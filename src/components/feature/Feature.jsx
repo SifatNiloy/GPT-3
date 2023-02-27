92512ac1ccb22424713a6be5ded784625be0227e
SifatNiloy <sifatniloy18@gmail.com>
2023-02-27 19:34:45 +0600

    folders and files created

---
+++ src/components/feature/Feature.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Feature.css'
+const Feature = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Feature;
---
+++ src/containers/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.jsx'
+const Footer = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Footer;
