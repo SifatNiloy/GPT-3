f6351f7dae0baa20b263c6b189cca8549c4e8a30
SifatNiloy <sifatniloy18@gmail.com>
2023-03-01 20:00:42 +0600

    completed top section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './Brand.css'
+import google from '../../assets/google.png'
+import slack from '../../assets/slack.png'
+import atlassian from '../../assets/atlassian.png'
+import dropbox from '../../assets/dropbox.png'
+import shopify from '../../assets/shopify.png'
 const Brand = () => {
     return (
-        <div>
-            
+        <div className='brands'>
+            <div className='images'>
+                <img src={google} alt="" />
+                <img src={slack} alt="" />
+                <img src={atlassian} alt="" />
+                <img src={dropbox} alt="" />
+                <img src={shopify} alt="" />
+            </div>
         </div>
     );
 };
