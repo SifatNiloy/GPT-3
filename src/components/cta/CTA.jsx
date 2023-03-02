874506cc7412919778c9a0b887a5e4efa332e220
SifatNiloy <sifatniloy18@gmail.com>
2023-03-02 22:49:25 +0600

    cta done

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import './CTA.css';
 const CTA = () => {
     return (
-        <div>
-            
+        <div className='cta'>
+            <div className='cta-left'>
+                <p>Request Early Access to Get Started</p>
+                <h1>Register today & start exploring the endless possiblities.</h1>
+            </div>
+            <div className='button-right'>
+                <button>Get Started</button>
+            </div>
         </div>
     );
 };
