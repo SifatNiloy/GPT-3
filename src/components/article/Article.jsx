de2c3d1b949f0f5b97b1f5220ac4a605d5dbc67e
SifatNiloy <sifatniloy18@gmail.com>
2023-03-02 23:14:26 +0600

    creating blogs section

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Article.css'
-const Article = () => {
+const Article = ({ imgs }) => {
     return (
         <div>
-            
+            <div>
+                <img src={imgs} alt="" />
+            </div>
+            <div className='box'>
+                <p>Sep 26, 2021</p>
+                <h2>GPT-3 and Open  AI is the future. Let us exlore how it is?</h2>
+                <p>Read Full Article</p>
+            </div>
         </div>
+
     );
 };
 
--- src/containers/Blog/Blog.jsx
+++ src/containers/Blog/Blog.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { blog1, blog2, blog3, blog4, blog5 } from './imports'
+import Article from '../../components/article/Article';
 import './Blog.css'
 const Blog = () => {
     return (
-        <div>
+        <div className='blog-section'>
+            <div>
+                <h1 className='gradient__text'>A lot is happening, <br />
+                    We are blogging about it.</h1>
+            </div>
+            <div className='blogs'>
+                <div>
+                    <Article imgs={blog1} />
+                    
+                </div>
+                <div className="blogs-group">
+                    <Article imgs={blog2} />
+                    <Article imgs={blog3} />
+                    <Article imgs={blog4} />
+                    <Article imgs={blog5} />
+                </div>
+            </div>
             
         </div>
     );
