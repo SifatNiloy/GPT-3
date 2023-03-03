8dab43aa17340fddadeb704e350ca2aaf45db0cf
SifatNiloy <sifatniloy18@gmail.com>
2023-03-03 23:06:26 +0600

    footer doing

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
-import './Footer.jsx'
+import './Footer.css'
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='footer'>
+            <div className='contents'>
+                <div className='footer-top'>
+                    <h1 className='gradient__text'>Do you want to step in to the future before others</h1>
+                    <div className='white-box'>
+                        <p>Request Early Access</p>
+                    </div>
+                </div>
+                <div className='text-links'>
+                    <div>
+                        <h1>GPT3</h1>
+                        <p>Crechterwoord K12 182 DK <br /> Alknjkcb, All Rights Reserved</p>
+                    </div>
+                    <div>
+                        <h5>Links</h5>
+                        <p>Overons</p>
+                        <p>Social Media</p>
+                        <p>Counters</p>
+                        <p>Contact</p>
+
+                    </div>
+                    <div>
+                        <h5>Company</h5>
+                        <p>Terms & Conditions</p>
+                        <p>Privacy Policy</p>
+                        <p>Contact</p>
+                    </div>
+                    <div>
+                        <h5>Get in touch</h5>
+                        <p>Crechterwoord K12 182 DK Alknjkcb</p>
+                        <p>085-132567</p>
+                        <p>info@payme.net</p>
+                    </div>
+
+
+                </div>
+            </div>
+            <p className='copyright'>© 2021 GPT-3. All rights reserved.</p>
         </div>
     );
 };
